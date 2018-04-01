c5d8fdb42dc0407afad1a1679d5ee56deaf07989
hugo.h <hugo.hadjur@grenoble-inp.org>
2018-04-01 21:41:42 +0900

    Simplified + adapted

--- network_insta/get_ids.py
+++ network_insta/get_ids.py
@@ -1,18 +1,28 @@
-from InstagramAPI import InstagramAPI
 import requests
+import csv
 
-# Get list of users ['user1', 'user2', ...]
-file = open('../get_data_from_scraper/usernames.txt', 'r')
-users = [line[:-1] for line in file.readlines()]
-users = users[260:]
 
-file = open('../get_data_from_scraper/ids2.txt', 'w')
+def get_ids(source_file_path, dest_file_path):
+    # Get list of users ['user1', 'user2', ...]
+    users = list()
+    with open(source_file_path, newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        for row in reader:
+            users.append(row[1])
+    users = users[1:]  # Remove column name
 
-# Go to specific user
-for user in users:
-    print(user)
-    url = 'https://www.instagram.com/' + user + '/?__a=1'
-    resp = requests.get(url=url)
-    data = resp.json()
+    file = open(dest_file_path, 'w')
 
-    file.write(data['graphql']['user']['id'] + '\n')
+    # Go to specific user
+    for user in users:
+        print(user)
+        url = 'https://www.instagram.com/' + user + '/?__a=1'
+        data = requests.get(url=url)
+        data = data.json()
+        file.write(data['graphql']['user']['id'] + '\n')
+
+
+if __name__ == '__main__':
+    # Store IDs in 'ids.txt'.
+    # Process it manually.
+    get_ids('../database/player_username_team_2017-18.csv', '../database/ids.txt')
